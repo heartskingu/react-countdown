e13881db59f51310374602ddd1f1b0e4a2ae5495
Anthony Toniolli <anthony.toniolli@mortgagepath.com>
2022-06-09 09:58:42 -0300

    Countdown prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [timerInput, setTimerInput] = useState("00:00:00");
+  const [timerList, setTimerList] = useState<string[]>([]);
+  const [selectedTimer, setSelectedTimer] = useState("00:00:00");
+  const [runningTimer, setRunningTimer] = useState<NodeJS.Timer>();
+
+  function addTimer(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    setTimerList((oldList) => [...oldList, timerInput]);
+    setTimerInput("00:00:00");
+  }
+
+  function timeToSeconds(time: string) {
+    const [hours = "0", minutes = "0", seconds = "0"] = time.split(":");
+    const hoursInSeconds = Number(hours) * 3600;
+    const minutesInSeconds = Number(minutes) * 60;
+    return hoursInSeconds + minutesInSeconds + Number(seconds);
+  }
+
+  function startCountdown() {
+    if (!selectedTimer) return;
+
+    let seconds = timeToSeconds(selectedTimer);
+    setRunningTimer(
+      setInterval(() => {
+        if (seconds-- > 0) {
+          const hour = String(Math.floor(seconds / 360)).padStart(2, "0");
+          const min = String(Math.floor(seconds / 60)).padStart(2, "0");
+          const sec = String(seconds % 60).padStart(2, "0");
+          setSelectedTimer(`${hour}:${min}:${sec}`);
+        } else {
+          clearInterval(runningTimer);
+        }
+      }, 1000)
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="timers-section">
+        <form onSubmit={addTimer}>
+          <input
+            type="time"
+            step={1}
+            name="timer"
+            id="timer"
+            placeholder="Add a timer"
+            value={timerInput}
+            onChange={(event) => setTimerInput(event.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {timerList.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                if (runningTimer) clearInterval(runningTimer);
+                setSelectedTimer(item);
+              }}
+            >
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="countdown-timer">
+        <div>
+          <span>{selectedTimer}</span>
+        </div>
+        <button onClick={() => startCountdown()}>Start</button>
+        <button onClick={() => clearInterval(runningTimer)}>Pause</button>
+      </section>
     </div>
   );
 }
